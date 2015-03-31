1a86a4f3de7e8eb05e74748c2a86245e63d3fd67
sparxcrossroads <kisshotblade@sina.com>
2015-03-31 11:12:33 +0800

    修改show scene的ui适应
    nexus gui.lable乱码 没解决

---
+++ Assets/script/show.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class show : MonoBehaviour {
+
+	// Use this for initialization
+    public GUITexture m_show;
+	void Start () {
+        m_show.pixelInset = new Rect(0, 0, Screen.width, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
